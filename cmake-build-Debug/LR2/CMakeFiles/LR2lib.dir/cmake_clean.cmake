7804b2f9533cde8d23e1b911e619082ae5b4d8c2
VladKon <vladkontsevykh@list.ru>
2020-04-27 18:43:43 +0300

    Lab 2 begin

---
+++ cmake-build-Debug/LR2/CMakeFiles/LR2lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "C:/Users/FLARTY/Desktop/Programs/Semester_4/AaDS/cmake-build-Debug/output/libLR2lib.a"
+  "C:/Users/FLARTY/Desktop/Programs/Semester_4/AaDS/cmake-build-Debug/output/libLR2lib.pdb"
+  "CMakeFiles/LR2lib.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LR2lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
