6a9b2db56397b3214fec4c043b25ce4f2cb14fc2
VladKon <vladkontsevykh@list.ru>
2020-05-31 13:41:31 +0300

    Beautification. Closed inputting test in a test sute.

--- CWTesting/GraphTest.cpp
+++ CWTesting/GraphTest.cpp
@@ -3,52 +3,54 @@
 
 SUITE (GraphTests) {
 
-	TEST_FIXTURE (Graph, TestGraph_inputCorrupted) {
-		CHECK_THROW (input ("inCorrupted.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputLackOfNumber) {
-		CHECK_THROW (input ("inLackofNumber.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputNoSource) {
-		CHECK_THROW (input ("inNoSource.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputNoDrain) {
-		CHECK_THROW (input ("inNoDrain.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputNegative) {
-		CHECK_THROW (input ("inNegative.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputLackOfNode) {
-		CHECK_THROW (input ("inLackOfNode.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputAsymmetry) {
-		CHECK_THROW (input ("inAsymmetry.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputEmptyLine) {
-		CHECK_THROW (input ("inEmptyLine.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_inputNoFile) {
-		CHECK_THROW (input ("something.txt"), std::invalid_argument);
-	}
-	
-	TEST_FIXTURE (Graph, TestGraph_input) {
-		input ("in.txt");
+	SUITE (inputTesting) {
+		TEST_FIXTURE (Graph, TestGraph_inputCorrupted) {
+			CHECK_THROW (input ("inCorrupted.txt"), std::invalid_argument);
+		}
 		
-		CHECK_EQUAL (4, nodes_->getLength());
+		TEST_FIXTURE (Graph, TestGraph_inputLackOfNumber) {
+			CHECK_THROW (input ("inLackofNumber.txt"), std::invalid_argument);
+		}
 		
-		CHECK_EQUAL (8, matrice_[0][1]);
-		CHECK_EQUAL (4, matrice_[0][2]);
-		CHECK_EQUAL (4, matrice_[1][2]);
-		CHECK_EQUAL (4, matrice_[1][3]);
-		CHECK_EQUAL (4, matrice_[3][2]);
+		TEST_FIXTURE (Graph, TestGraph_inputNoSource) {
+			CHECK_THROW (input ("inNoSource.txt"), std::invalid_argument);
+		}
+		
+		TEST_FIXTURE (Graph, TestGraph_inputNoDrain) {
+			CHECK_THROW (input ("inNoDrain.txt"), std::invalid_argument);
+		}
+		
+		TEST_FIXTURE (Graph, TestGraph_inputNegative) {
+			CHECK_THROW (input ("inNegative.txt"), std::invalid_argument);
+		}
+		
+		TEST_FIXTURE (Graph, TestGraph_inputLackOfNode) {
+			CHECK_THROW (input ("inLackOfNode.txt"), std::invalid_argument);
+		}
+		
+		TEST_FIXTURE (Graph, TestGraph_inputAsymmetry) {
+			CHECK_THROW (input ("inAsymmetry.txt"), std::invalid_argument);
+		}
+		
+		TEST_FIXTURE (Graph, TestGraph_inputEmptyLine) {
+			CHECK_THROW (input ("inEmptyLine.txt"), std::invalid_argument);
+		}
+		
+		TEST_FIXTURE (Graph, TestGraph_inputNoFile) {
+			CHECK_THROW (input ("something.txt"), std::invalid_argument);
+		}
+		
+		TEST_FIXTURE (Graph, TestGraph_input) {
+			input ("in.txt");
+			
+			CHECK_EQUAL (4, nodes_->getLength());
+			
+			CHECK_EQUAL (8, matrice_[0][1]);
+			CHECK_EQUAL (4, matrice_[0][2]);
+			CHECK_EQUAL (4, matrice_[1][2]);
+			CHECK_EQUAL (4, matrice_[1][3]);
+			CHECK_EQUAL (4, matrice_[3][2]);
+		}
 	}
 	
 	TEST (TestGraph_solveBasic) {
